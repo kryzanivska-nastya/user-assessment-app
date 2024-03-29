7b6b9044b1cf7ace9c93930918f654cf132714f2
Anastasia <155824290+kryzanivska-nastya@users.noreply.github.com>
2024-03-29 16:03:23 +0200

    setting up structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GraphComponent } from './graph/graph.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    GraphComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
